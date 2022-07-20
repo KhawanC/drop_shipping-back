781a55cd1b58ef16a349a940b13f6da7539d2d03
KhawanC <kauacassiano121@gmail.com>
2022-07-20 12:03:05 -0300

    fc

---
+++ src/main/java/com/br/egypto/drop_shipping/DropShippingApplication.java
@@ -0,0 +1,13 @@
+package com.br.egypto.drop_shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropShippingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/egypto/drop_shipping/DropShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.egypto.drop_shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
